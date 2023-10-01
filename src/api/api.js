e296cc1e3b81fe0cc68698990f2eb7b137bbfa51
Esteban <ecerdas_96@hotmail.com>
2023-09-30 19:09:28 -0600

    Arreglando API

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000" //"https://aluraflix-api-orcin.vercel.app/"  
+    baseURL: "https://aluraflix-api-orcin.vercel.app/" //"http://localhost:5000" //  
 })
 
 export const buscar = async(url, setData) => {
